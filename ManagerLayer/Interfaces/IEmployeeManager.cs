ee7df5a6173351b92ec96618c75bc33b2077f103
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-10 17:21:43 +0530

    [vaishnavi] added: business layer files

---
+++ ManagerLayer/Interfaces/IEmployeeManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Interfaces;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface IEmployeeManager
+    {
+
+        public bool AddEmployee(AddEmployeeModel model);
+        public EmployeeModel GetEmployeeById(int EmployeeId);
+        public List<EmployeeModel> GetAllEmployee();
+        public bool UpdateEmployee(EmployeeModel employee);
+        public bool DeleteEmployee(int? id);
+        public EmployeeModel Login(int employeeId, string employeeName);
+    }
+}
---
+++ ManagerLayer/Services/EmployeeManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Interfaces;
+
+namespace ManagerLayer.Services
+{
+    public class EmployeeManager:IEmployeeManager
+    {
+        private readonly IEmployeeRepository iemployee;
+        
+        public EmployeeManager(IEmployeeRepository employee)
+        {
+            this.iemployee = employee;
+
+        }
+        public bool AddEmployee(AddEmployeeModel model)
+        {
+            return iemployee.AddEmployee(model);
+        }
+        public EmployeeModel GetEmployeeById(int EmployeeId)
+        {
+            return iemployee.GetEmployeeById(EmployeeId);
+        }
+        public List<EmployeeModel> GetAllEmployee()
+        {
+            return iemployee.GetAllEmployee();
+        }
+
+        public bool UpdateEmployee(EmployeeModel employee)
+        {
+            return iemployee.UpdateEmployee(employee);
+        }
+        public bool DeleteEmployee(int? id)
+        {
+            return iemployee.DeleteEmployee(id);
+        }
+        public EmployeeModel Login(int employeeId, string employeeName)
+        {
+            return iemployee.Login(employeeId, employeeName);
+        }
+
+        
+    }
+}
